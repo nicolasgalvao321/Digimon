589f2f860cb211a5821b974de79829b578b1dd86
Nicolas Galvão <nicolas.9299@colegioprogresso.g12.br>
2026-02-25 11:55:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const digimonInput = document.getElementById("digimonInput");
+const buscarDigimon = document.getElementById("buscarDigimon");
+
+const CotarAcao = async () => {
+    const acao = digimonInput.value;
+
+    const response = await fetch(`https://digimon-api.vercel.app/api/digimon/name/${acao}`);
+    const data = await response.json();
+
+    const imagem = data[0].img;
+    const nome = data[0].name;
+    const level = data[0].level;
+
+    const card = document.getElementById("card");
+
+    card.innerHTML = `
+        <h2>${nome}</h2>
+        <img src="${imagem}" alt="${nome}">
+        <p>Nível: ${level}</p>
+    `;
+
+    card.style.display = "block";
+};
+
+buscarDigimon.addEventListener("click", CotarAcao);
